b6a7a43511c848b8a2b3e073a12c9d02d6005b2f
Luciano Janzkovski <luccas@gmail.com>
2020-11-23 08:00:02 -0300

    TypeORM start config

---
+++ src/database/migrations/1606107954629-CreateTablePedido.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class CreateTablePedido1606107954629
+  implements MigrationInterface {
+  // public async up(queryRunner: QueryRunner): Promise<void> {}
+  // public async down(queryRunner: QueryRunner): Promise<void> {}
+}
